ba89997d6b5f2c1b0668772cc1fc5a3a3c5185c4
Teemu Kolu <teemu.kolu@symbio.com>
2017-11-15 11:31:16 +0200

    Save favourites to shared preferences

--- app/src/main/java/com/oamk/beaconeggs/MainActivity.java
+++ app/src/main/java/com/oamk/beaconeggs/MainActivity.java
@@ -1,10 +1,13 @@
 package com.oamk.beaconeggs;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
@@ -89,6 +92,30 @@ public class MainActivity extends AppCompatActivity {
                 ((TextView) findViewById(R.id.restaurantTitle)).setText(text);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_favourites) {
+            Intent intent = new Intent(this, FavouritesActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
